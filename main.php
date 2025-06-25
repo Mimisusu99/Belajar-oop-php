d1d26e1b8d596eec278e35ab33969fad34910d8f
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-25 23:13:44 +0700

    Inheritance

--- main.php
+++ main.php
@@ -2,40 +2,65 @@
 
 //Constructor
 
-class Fruit{
+// class Fruit{
+//     private $name;
+//     private $color;
+//     private $price;
+
+//     function __construct($name, $color, $price) {
+//         $this->name = $name;
+//         $this->color = $color;
+//         $this->price = $price;
+//     }
+
+//     function introduce () {
+//         return $this->name . " - " . $this->color . " - " . $this->price;
+//     }
+
+//     function set_name($name) {
+//         $this->name = $name;
+//     }
+
+//     function get_name() {
+//         return $this->name;
+//     }
+
+//     function set_color($color) {
+//         $this->color = $color;
+//     }
+
+//     function get_color() {
+//         return $this->color;
+//     }
+// }
+
+// $apple = new Fruit("Apel", "Merah", 5000);
+
+// echo $apple->introduce();
+
+//=============================================
+
+//Inheritance
+
+class Vehicle{
     private $name;
     private $color;
-    private $price;
 
-    function __construct($name, $color, $price) {
+    function __construct($name, $color) {
         $this->name = $name;
         $this->color = $color;
-        $this->price = $price;
     }
 
-    function introduce () {
-        return $this->name . " - " . $this->color . " - " . $this->price;
-    }
-
-    function set_name($name) {
-        $this->name = $name;
-    }
-
-    function get_name() {
-        return $this->name;
-    }
-
-    function set_color($color) {
-        $this->color = $color;
-    }
-
-    function get_color() {
-        return $this->color;
+    function greet(){
+        return "Hello, I am a " . " - " . $this->color ."". $this->name;
     }
 }
 
-$apple = new Fruit("Apel", "Merah", 5000);
+class car extends Vehicle{
 
-echo $apple->introduce();
+}
+
+$car = new car("Mobil Esemka", "Transparan ");
+echo $car->greet();
 
 ?>
