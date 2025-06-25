1fec5daa35f33ef9d8c716606edb4d800780c4e8
Anshori Suhendro <anshorishndr@gmail.com>
2025-06-25 22:25:08 +0700

    encapsulation

--- main.php
+++ main.php
@@ -1,18 +1,37 @@
 <?php
 
+//Encapsulation
+
 class Fruit{
-    public $name;
-    public $color;
+    private $name;
+    private $color;
 
     function introduce () {
         return $this->name . " - " . $this->color;
     }
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+
+    function set_color($color) {
+        $this->color = $color;
+    }
+
+    function get_color() {
+        return $this->color;
+    }
 }
 
 $apple = new Fruit();
-$apple->name = "Apel";
-$apple->color = "Merah";
+$apple->set_name("Apel");
+$apple->set_color("Merah");
 
-echo $apple->introduce();
+echo $apple->get_name();
+echo $apple->get_color();
 
 ?>
